5692a908c01e50ea26f13caa8598ed277caa9765
PaulyDuk <me@paulyd.co.uk>
2025-07-15 20:13:31 +0100

    Added post view

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
